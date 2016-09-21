b883b0c6213d463e2a0cce1664330e28191dc189
zach larson <zlarson@gmail.com>
2016-09-20 20:48:10 -0500

    removed configs

--- config.py
+++ config.py
@@ -5,8 +5,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 facebook = {
-    "app_secret": 'd6d6fec7e52f533c31090be3f45ff022',
-    "app_id": '1124747760929464',
-    "page_id"      : "1821645501398664",
-    "access_token" : "EAAPZB877AirgBALtkZASEKhiccytjVZArDqOWCCCbCgr1fbi2KbUvzUStpxkxlJhCD1ZC23j78OFs0d3LpGSkMgtDghboOoWqv9WHcwAZAZBgZA2Q952NnvSFk39VFxaZARBmCLAWuI8FdwNMjJNJL2YXKDTj0tDYSltAVLT57dERQZDZD"
+    
     }
